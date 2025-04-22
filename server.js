33da72d95f3f0150a6b5979c3358e2befbf2aa61
Essar852 <essar@shopedge.tech>
2025-04-22 14:39:18 +0530

    Added CORS

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 server.use(jsonServer.bodyParser);
 server.use("/", router);
 
